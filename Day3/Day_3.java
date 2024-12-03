26452343b89221fcd1622752cd04f89f2ef2682d
Stilianos Papaspiros <stelios.papaspiros@gmail.com>
2024-12-03 19:51:21 +0200

    Day 3 Done
    Second part needs to be refactored ASAP. This should not be seen by any human being ever!!!

--- Day3/Day_3.java
+++ Day3/Day_3.java
@@ -9,12 +9,16 @@ public class Day_3 {
     public static void main(String[] args) {
         String filePath = "input.txt";
         String regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)";
+        String switchRegex = "(do\\(\\))|(don't\\(\\))|mul\\([0-9]{1,3},[0-9]{1,3}\\)";
+        String line = "";
+        Boolean enabled = true;
 
         List<String> multiplications = new ArrayList<>();
         int sum = 0;
         
         Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        
+        Pattern switchPattern = Pattern.compile(switchRegex, Pattern.MULTILINE);
+
         try {
             Scanner reader = new Scanner(new File(filePath));
             Scanner purifier;
@@ -24,6 +28,67 @@ public class Day_3 {
             matches.forEach(match -> multiplications.add(match.group()));
             
             for(String mult : multiplications){
+                mult = mult.replace("mul(", "").replace(",", " ").replace(")", "");
+                purifier = new Scanner(mult);
+                sum += purifier.nextInt() * purifier.nextInt();
+                
+                purifier.close();
+            }
+
+            reader.close();
+            
+            /**
+             * SECOND PART
+             * HOLLY FUCKING SHIT THIS GOD DAMN LANGUAGE AND ITS INPUT HANDLERS
+             */
+
+            sum = 0;
+            List<String> _multiplications = new ArrayList<>();
+            reader = new Scanner(new File(filePath));
+            line = reader.nextLine();
+            Scanner _line = new Scanner(line);
+            Matcher switchMatcher = switchPattern.matcher(line);
+            
+            int counter = 0;
+
+            while(reader.hasNextLine()){
+                while(switchMatcher.find()){
+                    if(switchMatcher.group().equals("don't()")){
+                        counter++;
+                        enabled = false;
+                    }
+                    else if(switchMatcher.group().equals("do()")){
+                        counter++;
+                        enabled = true;                        
+                    }
+                    else if(enabled){
+                        _multiplications.add(switchMatcher.group());
+                    }
+                }
+
+                line = reader.nextLine();
+                switchMatcher = switchPattern.matcher(line);
+                _line.close();
+                _line = new Scanner(line);
+            }
+
+            while(switchMatcher.find()){
+                if(switchMatcher.group().equals("don't()")){
+                    counter++;
+                    enabled = false;
+                }
+                else if(switchMatcher.group().equals("do()")){
+                    counter++;
+                    enabled = true;                        
+                }
+                else if(enabled){
+                    _multiplications.add(switchMatcher.group());
+                }
+            }
+
+            _line.close();
+
+            for(String mult : _multiplications){
                 mult = mult.replace("mul(", "").replace(",", " ").replace(")", "");
                 purifier = new Scanner(mult);
                 sum += purifier.nextInt() * purifier.nextInt();
